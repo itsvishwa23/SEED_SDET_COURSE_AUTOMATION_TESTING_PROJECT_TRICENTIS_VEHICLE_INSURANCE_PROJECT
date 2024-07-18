4459a7021d91bebd58de27f010f138859e1a89b3
VISHWANATH TAWARE <108166846+itsvishwa23@users.noreply.github.com>
2024-07-18 10:33:16 +0530

    :tada:

---
+++ SEED_SDET_COURSE_AUTOMATION_TESTING_PROJECT_TRICENTIS_VEHICLE_INSURANCE_PROJECT/src/main/java/SEED_SDET_COURSE_AUTOMATION_TESTING_PROJECT_TRICENTIS_VEHICLE_INSURANCE_PROJECT/SEED_SDET_COURSE_AUTOMATION_TESTING_PROJECT_TRICENTIS_VEHICLE_INSURANCE_PROJECT/App.java
@@ -0,0 +1,13 @@
+package SEED_SDET_COURSE_AUTOMATION_TESTING_PROJECT_TRICENTIS_VEHICLE_INSURANCE_PROJECT.SEED_SDET_COURSE_AUTOMATION_TESTING_PROJECT_TRICENTIS_VEHICLE_INSURANCE_PROJECT;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
